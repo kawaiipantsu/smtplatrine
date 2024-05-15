571b75285b505b2f19aeb9c8203eee25f82ca5fb
Kawaiipantsu <12233528+kawaiipantsu@users.noreply.github.com>
2024-05-15 22:17:28 +0200

    fixed script

--- contrib/database_test_import.sh
+++ contrib/database_test_import.sh
@@ -3,9 +3,9 @@
 ## Test scrpit to create a database and import tables from a sql file
 ## This script is not meant to be run in production
 
-$HOST = "localhost"
-$USER = "root"
-$DB = "import_eusdfjkvbsa"
+HOST = "localhost"
+USER = "root"
+DB = "import_eusdfjkvbsa"
 
 mysql -h "${HOST}" -u "${USER}" -p -e "CREATE DATABASE ${DB};"
 mysql -h "${HOST}" -u "${USER}" -p ${DB} < smtplatrine_database_tables.sql
